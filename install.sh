83e9c465851580b8d5b808ffbc48bf68b863d195
HeronErin <HeronErin@protonmail.com>
2025-07-31 14:41:11 -0400

    Less shit

--- install.sh
+++ install.sh
@@ -1,58 +1,53 @@
 #!/bin/bash
 
+set -euo pipefail
 
 # ==========================================
 #
-#        Desktop enviroment setup
+#        Desktop environment setup
 #
 # ==========================================
 
+LOC=$(dirname "$(realpath "$0")")
 
-LOC=$(dirname $(realpath $0))
+# Basic system packages
+sudo pacman -Syu --noconfirm
+sudo pacman -S --noconfirm nvim git base-devel man xorg-xrandr \
+                            konsole firefox blueman
 
-# Some packages
-sudo pacman -S nvim git base-devel man xorg-xrandr \
-              konsole firefox blueman
+# Enable Bluetooth service
+sudo systemctl enable --now bluetooth
 
+# Install Sublime Text
+curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/pacman.d/sublimehq.gpg > /dev/null
+sudo pacman-key --add /etc/pacman.d/sublimehq.gpg
+sudo pacman-key --lsign-key 8A8F901A
+echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | \
+    sudo tee -a /etc/pacman.conf
+sudo pacman -Syu --noconfirm sublime-text
 
-sudo systemctl enable bluetooth
-sudo systemctl start bluetooth
-
-# Sublime text
-
-curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
-echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
-sudo pacman -Syu sublime-text
-
-# Setup yay
+# Install yay
 cd /tmp
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
-makepkg -si
+makepkg -si --noconfirm
 
+# AUR packages
+yay -S --noconfirm gscreenshot
 
-
-yay -S gscreenshot
-
-
-cd $LOC
+# Return to original script location
+cd "$LOC"
 
 # Install config file symlinks
-ln .aliases ~/.aliases || true
-ln i3config ~/.config/i3/config || true
+mkdir -p ~/.config/i3
+ln -sf "$LOC/.aliases" ~/.aliases
+ln -sf "$LOC/i3config" ~/.config/i3/config
 
+# Install brightness scripts
+sudo install -m 755 getbright /bin/getbright
+sudo install -m 755 upbright /bin/upbright
+sudo install -m 755 downbright /bin/downbright
 
-# Simply brightness commands
-sudo cp getbright /bin/
-sudo cp upbright /bin/
-sudo cp downbright /bin/
-
-sudo chmod +x /bin/getbright
-sudo chmod +x /bin/upbright
-sudo chmod +x /bin/downbright
-
-
-
-# Patch dmenu to work with .aliases file
-sudo cp dmenu_path /bin/dmenu_path
-sudo cp dmenu_run /bin/dmenu_run
+# Patch dmenu scripts
+sudo install -m 755 dmenu_path /bin/dmenu_path
+sudo install -m 755 dmenu_run /bin/dmenu_run
